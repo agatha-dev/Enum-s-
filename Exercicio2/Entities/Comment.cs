b8cd58a9bea6fe7c9f189f76c1d2c499a1dba084
Ágatha Almeida <74624597+agatha-dev@users.noreply.github.com>
2022-08-28 17:39:05 -0300

    Criando solução de Post Comment

---
+++ Exercicio2/Entities/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2.Entities
+{
+    public class Comment
+    {
+        public Comment(string text)
+        {
+            Text = text;
+        }
+        public Comment()
+        {
+
+        }
+
+        public string Text { get; set; }
+
+
+    }
+}
