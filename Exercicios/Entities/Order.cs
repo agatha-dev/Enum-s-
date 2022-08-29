718d44ad764344e49b76e6c5a921fd6319809dd0
Ágatha Almeida <74624597+agatha-dev@users.noreply.github.com>
2022-08-28 22:52:38 -0300

    Criação do treinamento de Herança

---
+++ Exercicios/Entities/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.Entities
+{
+    public class Order
+    {
+        public Order(DateTime moment, string orderStatus, Client client)
+        {
+            Moment = moment;
+            OrderStatus = orderStatus;
+            Client = client;
+        }
+
+        public DateTime Moment { get; set; }
+        public string OrderStatus { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public void AddItems(OrderItem orderItem)
+        {
+            Items.Add(orderItem);
+        }
+        public void RemoveItems(OrderItem orderItem)
+        {
+            Items.Remove(orderItem);
+        }
+    }
+}
---
+++ Herança/Entities/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herança.Entities
+{
+    public class Account
+    {
+        public int Number { get; private set; } //Não pode ser alterado
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; } //Só pode ser alterado pela classe ou sub classe
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void WithDraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
