0e751d68bddfad73b74f827344903a5d5c336b3b
Ágatha Almeida <74624597+agatha-dev@users.noreply.github.com>
2022-08-30 21:50:10 -0300

    Terminando Classe

---
+++ Herança/Entities/SavingAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herança.Entities
+{
+    public class SavingAccount : Account
+    {
+        public double InterestRate { get; set; }
+        public SavingAccount()
+        {
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interestRate) 
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+        
+        public void UpdateBalace()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
